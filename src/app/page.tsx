1af07583fe26265c427a4bdad6e1e46ab8a4a002
Dinaka Nwamu <dinakanwamu@Dinakas-MacBook-Pro.local>
2024-01-17 16:31:48 +0100

    Clerk and UI update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,48 @@
-'use client'
-import { Chat } from '@/components/chat'
-import CourseSelector from '@/components/course-selector'
-import { ModeToggle } from '@/components/dark-mode-toggle'
-import { Button } from '@/components/ui/button'
-import { SignIn, UserButton, auth } from '@clerk/nextjs'
-import Link from 'next/link'
+"use client";
+import { Chat } from "@/components/chat";
+import CourseSelector from "@/components/course-selector";
+import { ModeToggle } from "@/components/dark-mode-toggle";
+import { Button } from "@/components/ui/button";
+import { SignIn, UserButton, auth } from "@clerk/nextjs";
+import Link from "next/link";
 import { useSession } from "@clerk/nextjs";
-import { useCourseContext } from '@/app/contexts/chosencourse-context';
-export default  function Home() {
+import { useCourseContext } from "@/app/contexts/chosencourse-context";
+export default function Home() {
   const { isLoaded, session, isSignedIn } = useSession();
-const {isCourseChosen,setIsCourseChosen} = useCourseContext()
+  const { isCourseChosen, setIsCourseChosen } = useCourseContext();
 
   return (
     <main className="relative container flex min-h-screen flex-col">
-    <div className=" p-4 flex h-14 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
-      <span className="font-bold">AI VSA</span>
-<div className='flex items-center gap-2'>   <ModeToggle />   {
-    isSignedIn ? <UserButton afterSignOutUrl='/' />  : ''
-  } 
-</div> 
-
-
-     </div>
-     <div className="flex flex-1 py-4">
-  <div className="w-full">
-    {isSignedIn  ?<div>
-      { isCourseChosen ? <Chat/>:
-          <CourseSelector />
-      }
-    
-    </div> :   <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <Link href='/sign-in'>
-  <Button >Login to get started </Button></Link>
-    </div>}
-  </div>
-</div>
-
+      <div className=" p-4 flex h-14 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
+        <span className="font-bold">AI VSA</span>
+        <div className="flex items-center gap-2">
+          {" "}
+          <ModeToggle /> {isSignedIn ? <UserButton afterSignOutUrl="/" /> : ""}
+        </div>
+      </div>
+      <div className="flex flex-1 py-4">
+        <div className="w-full">
+          {isSignedIn ? (
+            <div>{isCourseChosen ? <Chat /> : <CourseSelector />}</div>
+          ) : (
+            <div>
+              <div className="text-center mt-24 ">
+                <h1 className="md:text-8xl text-4xl font-semibold mb-4 ">
+                  Welcome to BUBOT
+                </h1>
+                <p className="bg-gray-700 text-white italic  inline p-2 rounded-xl ">
+                  Your AI Virtual Study Assistant
+                </p>
+              </div>
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <Link href="/sign-in">
+                  <Button>Login to get started </Button>
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </main>
-  ) }
+  );
+}
